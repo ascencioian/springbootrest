0ba0d59bae069f0ca038d50e19ef71915d0261e4
Ian Ascencio <ian.ascencio.mancia@gmail.com>
2022-06-10 11:52:49 -0400

    modified index.html for thymeleaf
    inserted database content to index page

--- src/main/java/com/project/controller/IndexController.java
+++ src/main/java/com/project/controller/IndexController.java
@@ -1,15 +1,26 @@
 package com.project.controller;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.project.service.SneakerService;
+
 @Controller
 public class IndexController {
 	
+	@Autowired
+	private SneakerService repo;
+	
 	@GetMapping("/")
 	public String home(Model model) {
+		
+		
 		model.addAttribute("something", "this is something");
+		model.addAttribute( "sneaker", repo.getSneakers());
 		return "index";
 	}
 
