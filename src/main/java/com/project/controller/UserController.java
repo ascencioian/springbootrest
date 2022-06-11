bea4082b1948cdf4127620f94c0b788d7cdcc2c9
Ian Ascencio <ian.ascencio.mancia@gmail.com>
2022-06-11 14:28:16 -0400

    implemented crud for user model

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -1,5 +1,60 @@
 package com.project.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.entity.Sneaker;
+import com.project.entity.User;
+import com.project.service.SneakerService;
+import com.project.service.UserService;
+
+@RestController
 public class UserController {
+		
+		@Autowired
+		private UserService userRepo;
+		
+			//-----------------------------------get
+		
+			@GetMapping("/users")
+			public List<User> getUsers(){
+				System.out.println("getUsers called");
+
+				return userRepo.getUsers();
+			}
+			
+			@GetMapping("/user/{email}")
+			public Optional<User> getUser(@PathVariable("email") String email){
+		
+				System.out.println("getUser called");
+				return userRepo.getUser(email);
+				
+			}
+			
+			//-----------------------------------post
+			@PostMapping("/user")
+			public User createUser(@RequestBody User u1) {
+				System.out.println("createUser called");
+				return userRepo.createUser(u1);
+				
+			}
+			
+			//------------------------------------delete
+			@DeleteMapping("/user/{email}")
+			public void deleteSneaker(@PathVariable("email") String email) {
+				
+				System.out.println("deleteUser called");
+				userRepo.deleteUser(email);
+			}
+			
+			//-------------------------------------put
 
 }
--- src/main/java/com/project/service/UserServiceImpl.java
+++ src/main/java/com/project/service/UserServiceImpl.java
@@ -4,27 +4,33 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.project.entity.Sneaker;
 import com.project.entity.User;
 import com.project.repository.UserRepository;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository repo;
 
+	//---------------------get----------------------
+	
 	@Override
 	public List<User> getUsers() {
 		// TODO Auto-generated method stub
-		return null;
+		 return (List<User>) repo.findAll();
 	}
 
 	@Override
 	public Optional<User> getUser(String email) {
 		// TODO Auto-generated method stub
-		return null;
+		return repo.findById(email);
 	}
 
+	//--------------------post-------------------------
 	@Override
 	public User createUser(User u1) {
 		// TODO Auto-generated method stub
@@ -32,11 +38,13 @@ public class UserServiceImpl implements UserService {
 		
 		return u1;
 	}
+	
+	//----------------------delete------------------
 
 	@Override
 	public void deleteUser(String email) {
 		// TODO Auto-generated method stub
-		
+		repo.deleteById(email);
 	}
 
 }
